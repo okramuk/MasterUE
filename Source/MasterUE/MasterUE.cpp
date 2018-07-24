85a3322d5e90f8962451ad433ea6135e84d9b069
m.alvarez <okramuk@gmail.com>
2018-07-23 22:21:07 -0500

    Inicial commit

---
+++ Source/MasterUE/MasterUE.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MasterUE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MasterUE, "MasterUE" );
